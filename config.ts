954ddd1505604b708066f6e12491e83159f47a8b
Ferdous Bhai <ferdousbd@gmail.com>
2023-03-28 12:08:21 +0800

    Add humor to DAN

--- config.ts
+++ config.ts
@@ -1,3 +1,2 @@
 export const openaiKey = Deno.env.get("OPENAI_API_KEY")!;
 export const botToken = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
-export const REDIS_URL = Deno.env.get("REDIS_URL") || "redis://127.0.0.1:6379";
